f39bdac9b5f8b46eccc8933a753d7e5f6b70f90d
Angel <angel.gtz.pc.527@gmail.com>
2024-02-25 17:59:25 -0600

    Registro de maestros hecho

--- sistema_fcc_api/urls.py
+++ sistema_fcc_api/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from sistema_fcc_api.views import bootstrap
 from sistema_fcc_api.views import users
 from sistema_fcc_api.views import auth
-from sistema_fcc_api.views import alumno
+from sistema_fcc_api.views import alumnos
+from sistema_fcc_api.views import maestros
 
 urlpatterns = [
     #Version
@@ -26,7 +27,9 @@ urlpatterns = [
     #Create Admin
         path('admin/', users.AdminView.as_view()),
     #Create Alumno
-        path('alumno/', alumno.AlumnoView.as_view()),
+        path('alumno/', alumnos.AlumnoView.as_view()),
+    #Create Maestro
+        path('maestro/', maestros.MaestroView.as_view()),
     #Login
         path('token/', auth.CustomAuthToken.as_view()),
     #Logout
